b84803cb7573641dd55a1cc3cfa532366cbe7ead
Cheffie <80277939+Cheffie-NA@users.noreply.github.com>
2023-07-08 12:07:39 -0400

    Add files via upload

--- Limbus Company/src/teamfinder/SanityEffect.java
+++ Limbus Company/src/teamfinder/SanityEffect.java
@@ -1,5 +1,5 @@
 package teamfinder;
 
 public class SanityEffect {
-
+	
 }
--- Limbus Company/src/teamfinder/Sinner.java
+++ Limbus Company/src/teamfinder/Sinner.java
@@ -1,12 +1,16 @@
 package teamfinder;
 
 public class Sinner {
-	int minHP;
-	int maxHP;
-	int minDF;
-	int maxDF;
-	SkillToken token1;
-	SkillToken token2;
-	SkillToken token3;
+	String name; //name of sinner
+	int minHP; //level 1 health
+	int maxHP; // level 35* health
+	int minDF; //level 1 defense
+	int maxDF; //level 35* defense
+	int rarity; // 1, 2 or 3
+	SkillToken token1; //First skill
+	SkillToken token2; //Second skill
+	SkillToken token3; //Third skill
+	Passive teamPassive; //passive in combat
+	Passive supportPassive; //passive out of combat
 	
 }
